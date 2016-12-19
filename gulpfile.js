b5db5242055eb1a29c7b734bb4d2a68a4d733063
nverba <distantstate@gmail.com>
2016-12-19 12:26:50 +0000

    output string

--- gulpfile.js
+++ gulpfile.js
@@ -7,36 +7,47 @@ var parseFuncs = fileParser({
   name: 'gulp-jeml',
   func: (body) => {
     let funcs = body.match(/^func\s+(\w+).+$/mg) || [];
-    funcs.map(func => {
+    funcs = funcs.map(func => {
       let Ob = {};
       Ob.FnName = func.match(/^func\s+(\w+)/)[1];
       Ob.public = (/[A-Z]/).test(Ob.FnName);
-      Ob.types  = func.match(/(\S+)(?:,)/g) || [];
-      Ob.types.push(func.match(/(?:\w+\(.*?)(\w+\))/) ? func.match(/(?:\w+\(.*?)(\w+)(?:\))/)[1] : []);  // (?:\)
-      Ob.returnType = func.match(/\)\s+(\w+)\s+{/) ?  func.match(/\)\s+(\w+)\s+{/)[1] : false
-      console.log(Ob)
+      Ob.types  = func.match(/(\S+)(?:,)/g) ? func.match(/(\S+)(?:,)/g).map(type => type.replace(',', '')) : [];
+      Ob.types.push(func.match(/(?:\w+\(.*?)(\S+\))/) ? func.match(/(?:\w+\(.*?)(\S+)(?:\))/)[1] : []);  // (?:\)
+      Ob.returnType = func.match(/\)\s+(.+)\s+{/) ? func.match(/\)\s+(.+)\s+{/)[1].replace(/[\(\)]/g, '').split(', ') : false
       return Ob;
     });
 
     let structs = body.match(/^func\s+\(.+\)/mg) || [];
-    structs.map(struct => {
+    structs = structs.map(struct => {
       let Ob = {};
       Ob.FnName = struct.match(/(\w+)(?:\()/)[1];
       Ob.public = (/[A-Z]/).test(Ob.FnName);
       Ob.struct = struct.match(/func\s\(\w+\s(\w+)/)[1];
-      Ob.types  = struct.match(/(\S+)(?:,)/g) || [];
-      Ob.types.push(struct.match(/(?:\w+\(.*?)(\w+\))/) ? struct.match(/(?:\w+\(.*?)(\w+\))/)[1] : []);
-      Ob.returnType = struct.match(/\)\s+(\w+)\s+{/) ?  struct.match(/\)\s+(\w+)\s+{/)[1] : false
-      console.log(Ob)
+      Ob.types  = struct.match(/(\S+)(?:,)/g) ? struct.match(/(\S+)(?:,)/g).map(type => type.replace(',', '')) : [];
+      Ob.types.push(struct.match(/(?:\w+\(.*?)(\S+\))/) ? struct.match(/(?:\w+\(.*?)(\S+\))/)[1] : []);
+      Ob.returnType = struct.match(/\)\s+(.+)\s+{/) ?  struct.match(/\)\s+(.+)\s+{/)[1].replace(/[\(\)]/g, '').split(', ') : false
       return Ob;
     });
 
-    console.log(structs)
-    console.log(funcs)
+    function fnString(Fn) {
+      console.log(Fn)
+      return `${ Fn.public ? '+' : '-' } ${ Fn.FnName } (${ Fn.types ? Fn.types.join(', ') : '' }) : ${  Fn.returnType ? '(' + Fn.returnType.join(', ') + ')' : 'void' }`
+    }
 
-      return 'funcs'
-  extension: '.go'
+    // console.log(structs)
+    // console.log(funcs)
+    let output = '';
+    structs.forEach(struct => {
+      output += `${ fnString(struct)  } \n`
+    })
+
+    funcs.forEach(func => {
+      output += `${ fnString(func)  } \n`
+    })
+
+    return output
   },
+  extension: '.go'
 });
 
 gulp.task('go', [], function () {
