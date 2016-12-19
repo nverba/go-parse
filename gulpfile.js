1e6f94cd666c6d332e220a5ff4cfb4ff76cd8efe
nverba <distantstate@gmail.com>
2016-12-19 14:38:16 +0000

    refactor parser - fixes all errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,88 @@
 var gulp = require('gulp');
-// var requireDir = require('require-dir')('./tasks');
+var del = require('del');
 var fileParser = require('gulp-file-parser');
 
 
 var parseFuncs = fileParser({
   name: 'gulp-jeml',
   func: (body) => {
-    let funcs = body.match(/^func\s+(\w+).+$/mg) || [];
-    funcs = funcs.map(func => {
-      let Ob = {};
-      Ob.FnName = func.match(/^func\s+(\w+)/) ? func.match(/^func\s+(\w+)/)[1] : '';
-      Ob.public = (/[A-Z]/).test(Ob.FnName);
-      Ob.types  = func.match(/(\S+)(?:,)/g) ? func.match(/(\S+)(?:,)/g).map(type => type.replace(',', '')) : [];
-      Ob.types.push(func.match(/(?:\w+\(.*?)(\S+\))/) ? func.match(/(?:\w+\(.*?)(\S+)(?:\))/)[1] : []);  // (?:\)
-      Ob.returnType = func.match(/\)\s+(.+)\s+{/) ? func.match(/\)\s+(.+)\s+{/)[1].replace(/[\(\)]/g, '').split(', ') : false
-      return Ob;
-    });
 
-    let structs = body.match(/^func\s+\(.+\)/mg) || [];
-    structs = structs.map(struct => {
-      let Ob = {};
-      Ob.FnName = struct.match(/(\w+)(?:\()/) && struct.match(/(\w+)(?:\()/)[1];
-      Ob.public = (/[A-Z]/).test(Ob.FnName);
-      Ob.struct = struct.match(/func\s\(\w+\s(\w+)/) && struct.match(/func\s\(\w+\s(\w+)/)[1];
-      Ob.types  = struct.match(/(\S+)(?:,)/g) ? struct.match(/(\S+)(?:,)/g).map(type => type.replace(',', '')) : [];
-      Ob.types.push(struct.match(/(?:\w+\(.*?)(\S+\))/) ? struct.match(/(?:\w+\(.*?)(\S+\))/)[1] : []);
-      Ob.returnType = struct.match(/\)\s+(.+)\s+{/) ?  struct.match(/\)\s+(.+)\s+{/)[1].replace(/[\(\)]/g, '').split(', ') : false
+    let funcs = body.match(/^func\s+.+?{\s*$/mg) || [];
+
+      funcs = funcs.map(func => {
+
+      let firstParens          = func.match(/(?:func\s+)(\(.+?\))/);
+      let name                 = func.match(/(\w+)(?:\()/)[1];
+      let secondParens         = func.match(/(?:\w+)(\(.*?\))/)[1];
+      let thirdParensOrString  = (match => match && match[1])(func.match(/(?:\w+\(.+?\)\s+)(.+?)(?:\s+{)/));
+
+      let Ob         = {};
+      Ob.name        = name;
+      Ob.public      = (/[A-Z]/).test(name[0]);
+      Ob.types       = (matches => matches && matches.map(type => type.replace(',', '')))(secondParens.match(/(\S+)(?:,)/g));   
+      Ob.returnTypes = thirdParensOrString;
+
       return Ob;
     });
 
     function fnString(Fn) {
-      console.log(Fn)
-      return `${ Fn.public ? '+' : '-' } ${ Fn.FnName } (${ Fn.types ? Fn.types.join(', ') : '' }) : ${  Fn.returnType ? '(' + Fn.returnType.join(', ') + ')' : 'void' }`
+      return `${ Fn.public ? '+' : '-' } ${ Fn.name } (${ Fn.types ? Fn.types.join(', ') : '' }) : ${ Fn.returnTypes || 'void' }`
+
+      //       return `${ Fn.public ? '+' : '-' } ${ Fn.FnName } (${ Fn.types ? Fn.types.join(', ') : '' }) : ${  Fn.returnType ? '(' + Fn.returnType.join(', ') + ')' : 'void' }`
+
     }
 
-    // console.log(structs)
-    // console.log(funcs)
     let output = '';
-    structs.forEach(struct => {
-      output += `${ fnString(struct)  } \n`
-    })
-
     funcs.forEach(func => {
       output += `${ fnString(func)  } \n`
     })
-
     return output
   },
   extension: '.go'
 });
 
 gulp.task('go', [], function () {
-  return gulp.src('./src/**/*.go', '!./src/**/*_test.go')
+  return gulp.src(['./src/**/*.go', '!./src/**/*_test.go'])
     .pipe(parseFuncs())
+    .on('error', console.error.bind(console))
     .pipe(gulp.dest('./docs'));
 });
 
-gulp.task('default', ['go'], function () {
+gulp.task('default', ['clean:docs', 'go'], function () {
   // place code for your default task here
 });
+
+gulp.task('clean:docs', function () {
+  return del([
+    'docs/**/*'
+  ]);
+});
+
+
+
+// (\s)(?:,|\))
+// (email string)
+// (w mango.Wallet)
+// (wt Wallet)
+// (currency string, c *ctx.Context)
+// (c *ctx.Context)
+// (c *ctx.Context)
+// (wallets []Wallet, currency string)
+// ()
+// (name string)
+// (email string)
+// (ds string)
+// (mp map[string]string, keys []string)
+// (id string, c *ctx.Context)
+// (rid string, c *ctx.Context)
+// ()
+// ()
+// (pi *PayIns, ps PayInSeller, aid, fromw, tow string)
+// (pi *PayIns, sellers []Seller, c *ctx.Context)
+// (c *ctx.Context)
+// (tid string, c *ctx.Context)
+// (c *ctx.Context)
+// (c *ctx.Context)
+// (c *ctx.Context)
+// (c *ctx.Context)
+// (c *ctx.Context, tid int64)
