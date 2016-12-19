07d5c03bd26a8a2f981b2000be59ab8c64b05caf
nverba <distantstate@gmail.com>
2016-12-19 12:36:05 +0000

    ready for testing

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var parseFuncs = fileParser({
     let funcs = body.match(/^func\s+(\w+).+$/mg) || [];
     funcs = funcs.map(func => {
       let Ob = {};
-      Ob.FnName = func.match(/^func\s+(\w+)/)[1];
+      Ob.FnName = func.match(/^func\s+(\w+)/) ? func.match(/^func\s+(\w+)/)[1] : '';
       Ob.public = (/[A-Z]/).test(Ob.FnName);
       Ob.types  = func.match(/(\S+)(?:,)/g) ? func.match(/(\S+)(?:,)/g).map(type => type.replace(',', '')) : [];
       Ob.types.push(func.match(/(?:\w+\(.*?)(\S+\))/) ? func.match(/(?:\w+\(.*?)(\S+)(?:\))/)[1] : []);  // (?:\)
@@ -20,9 +20,9 @@ var parseFuncs = fileParser({
     let structs = body.match(/^func\s+\(.+\)/mg) || [];
     structs = structs.map(struct => {
       let Ob = {};
-      Ob.FnName = struct.match(/(\w+)(?:\()/)[1];
+      Ob.FnName = struct.match(/(\w+)(?:\()/) && struct.match(/(\w+)(?:\()/)[1];
       Ob.public = (/[A-Z]/).test(Ob.FnName);
-      Ob.struct = struct.match(/func\s\(\w+\s(\w+)/)[1];
+      Ob.struct = struct.match(/func\s\(\w+\s(\w+)/) && struct.match(/func\s\(\w+\s(\w+)/)[1];
       Ob.types  = struct.match(/(\S+)(?:,)/g) ? struct.match(/(\S+)(?:,)/g).map(type => type.replace(',', '')) : [];
       Ob.types.push(struct.match(/(?:\w+\(.*?)(\S+\))/) ? struct.match(/(?:\w+\(.*?)(\S+\))/)[1] : []);
       Ob.returnType = struct.match(/\)\s+(.+)\s+{/) ?  struct.match(/\)\s+(.+)\s+{/)[1].replace(/[\(\)]/g, '').split(', ') : false
@@ -51,9 +51,9 @@ var parseFuncs = fileParser({
 });
 
 gulp.task('go', [], function () {
-  return gulp.src('./test.go')
+  return gulp.src('./src/**/*.go', '!./src/**/*_test.go')
     .pipe(parseFuncs())
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./docs'));
 });
 
 gulp.task('default', ['go'], function () {
